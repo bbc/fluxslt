4023279e06a2487b816a15486250eea9c1ed4724
blam <benjamin.lambert@bbc.co.uk>
2015-08-21 00:02:19 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -24,8 +24,25 @@ function xsltproc(stylesheetPath, untransformedContent) {
     });
 }
 
+function getPathForStylesheet() {
+    if (this.stylesheetPath) {
+        return Promise.resolve(this.stylesheetPath);
+    } else {
+        return tmp.dirAsync()
+            .get(0)
+            .bind(this)
+            .then(function(path) {
+                this.stylesheetPath = path + '/stylesheet.xsl';
+                return fs.writeFileAsync(this.stylesheetPath, this.stylesheet, 'utf-8');
+            })
+            .then(function() {
+                return this.stylesheetPath;
+            });
+    }
+}
+
 function run(resolve, reject) {
-    if (!this.stylesheet) {
+    if (!this.stylesheet && !this.stylesheetPath) {
         return reject(new Error('No stylesheet has been loaded.'));
     }
 
@@ -33,15 +50,10 @@ function run(resolve, reject) {
         return reject(new Error('No content has been loaded.'));
     }
 
-    return tmp.dirAsync()
-        .get(0)
+    return getPathForStylesheet.call(this)
         .bind(this)
         .then(function(path) {
-            this.stylesheetPath = path + '/stylesheet.xsl';
-            return fs.writeFileAsync(this.stylesheetPath, this.stylesheet, 'utf-8');
-        })
-        .then(function() {
-            return xsltproc(this.stylesheetPath, this.content);
+            return xsltproc(path, this.content);
         })
         .then(resolve);
 }
@@ -52,11 +64,12 @@ function fluxslt() {
             this.stylesheet = stylesheet;
             return this;
         },
-        withContent: function(content) {
-            this.content = content;
+        withStylesheetPath: function(stylesheetPath) {
+            this.stylesheetPath = stylesheetPath;
             return this;
         },
-        run: function() {
+        runOn: function(content) {
+            this.content = content;
             return new Promise(run.bind(this));
         }
     }
