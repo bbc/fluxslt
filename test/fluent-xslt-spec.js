dd762108fff9dabeab38448c1b86c5f16ccc34b8
blam <benjamin.lambert@bbc.co.uk>
2015-08-20 20:19:03 +0200

    Updating tests

---
+++ test/fluent-xslt-spec.js
@@ -0,0 +1,34 @@
+describe('Fluent XSLT', function() {
+
+    var fluxslt = require('../'),
+        stylesheet,
+        expectedOutput,
+        fs = require('fs'),
+        content;
+
+    beforeEach(function() {
+        stylesheet = fs.readFileSync(__dirname + '/fixtures/stylesheet.xsl');
+        content = fs.readFileSync(__dirname + '/fixtures/content.xml');
+        expectedOutput = fs.readFileSync(__dirname + '/fixtures/expected.xml');
+    })
+
+    it('should throw an error when no stylesheet is supplied', function(done) {
+        fluxslt()
+            .run()
+            .catch(function(error){
+                expect(error.message).toBe('No stylesheet has been loaded.');
+                done();
+            });
+    });
+
+    it('should throw an error when no content is supplied', function(done) {
+        fluxslt()
+
+            .run()
+            .catch(function(error){
+                expect(error.message).toBe('No stylesheet has been loaded.');
+                done();
+            });
+    });
+
+})
