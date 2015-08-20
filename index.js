1f3f771234549db07d8263b0841e5d6a8ae28dbc
blam <benjamin.lambert@bbc.co.uk>
2015-08-21 00:03:12 +0200

    Updating index.js

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var Promise = require('bluebird'),
+    fs = Promise.promisifyAll(require('fs'))
+    tmp = Promise.promisifyAll(require('tmp'));
+
+function xsltproc(stylesheetPath, untransformedContent) {
+    var spawn = require('child_process').spawn;
+    return new Promise(function(resolve, reject) {
+        var cmd = spawn('/usr/bin/xsltproc', [stylesheetPath, '-']),
+            buffer = [];
+
+        cmd.stdout.on('data', function(data) {
+            buffer.push(data);
+        });
+
+        cmd.stdout.on('close', function(code) {
+            if (!code) {
+                resolve(Buffer.concat(buffer).toString());
+            } else {
+                reject(new Error('Exited with code: ' + code));
+            }
+        });
+        cmd.stdin.write(untransformedContent);
+        cmd.stdin.end();
+    });
+}
+
 function run(resolve, reject) {
     if (!this.stylesheet) {
         return reject(new Error('No stylesheet has been loaded.'));
@@ -7,11 +33,20 @@ function run(resolve, reject) {
         return reject(new Error('No content has been loaded.'));
     }
 
-    return resolve(this.content);
+    return tmp.dirAsync()
+        .get(0)
+        .bind(this)
+        .then(function(path) {
+            this.stylesheetPath = path + '/stylesheet.xsl';
+            return fs.writeFileAsync(this.stylesheetPath, this.stylesheet, 'utf-8');
+        })
+        .then(function() {
+            return xsltproc(this.stylesheetPath, this.content);
+        })
+        .then(resolve);
 }
 
 function fluxslt() {
-    var Promise = require('bluebird');
     return  {
         withStylesheet: function(stylesheet) {
             this.stylesheet = stylesheet;
